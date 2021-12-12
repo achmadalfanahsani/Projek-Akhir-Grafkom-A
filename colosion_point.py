f4b57ef3f469de487303c4d7e60738a108b5d88b
achmadalfanahsani <90007108+achmadalfanahsani@users.noreply.github.com>
2021-12-12 09:32:21 +0700

    Add files via upload
    data colosion point (awal)

---
+++ colosion_point.py
@@ -0,0 +1,114 @@
+from ctypes import BigEndianStructure
+import OpenGL.GL
+from OpenGL.GL.glget import GLsize
+import OpenGL.GLUT
+import OpenGL.GLU
+
+# program pertama
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def point1():
+    glPushMatrix()
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(36,349)
+    glVertex2f(53,349)
+    glVertex2f(53,366)
+    glVertex2f(36,366)
+    glEnd()
+    glPopMatrix()
+
+
+def point2():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(36,24)
+    glVertex2f(53,24)
+    glVertex2f(53,41)
+    glVertex2f(36,41)
+    glEnd()
+
+
+def point3():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(189,199)
+    glVertex2f(206,199)
+    glVertex2f(206,216)
+    glVertex2f(189,216)
+    glEnd()
+
+
+def point4():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(194,165)
+    glVertex2f(211,165)
+    glVertex2f(211,182)
+    glVertex2f(194,182)
+    glEnd()
+
+def point5():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(172,106)
+    glVertex2f(189,106)
+    glVertex2f(189,123)
+    glVertex2f(172,123)
+    glEnd()
+
+
+def point6():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(282,199)
+    glVertex2f(299,199)
+    glVertex2f(299,216)
+    glVertex2f(282,216)
+    glEnd()
+
+
+
+def point7():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(137,305)
+    glVertex2f(154,305)
+    glVertex2f(154,322)
+    glVertex2f(137,322)
+    glEnd()
+
+def point8():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(246,56)
+    glVertex2f(263,56)
+    glVertex2f(263,73)
+    glVertex2f(246,73)
+    glEnd()
+
+def point9():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(343,22)
+    glVertex2f(360,22)
+    glVertex2f(360,39)
+    glVertex2f(343,39)
+    glEnd()
+
+def point10():
+    glBegin(GL_QUADS)
+    glColor3ub(0, 255, 255)
+    glVertex2f(320,270)
+    glVertex2f(337,270)
+    glVertex2f(337,287)
+    glVertex2f(320,287)
+    glEnd()
+
+
+
+
+
+
